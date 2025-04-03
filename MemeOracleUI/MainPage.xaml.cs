7a6956372ebffc2baeafdb913e026bedfcfc98ef
Landon Pantoja <153697529+landon0595@users.noreply.github.com>
2025-04-03 17:46:54 -0400

    Capston App Test.3

--- MemeOracleUI/MainPage.xaml.cs
+++ MemeOracleUI/MainPage.xaml.cs
@@ -63,7 +63,7 @@ namespace MemeOracleUI
                 <style>
                   html, body {{
                     margin: 0; padding: 0; height: 100%; width: 100%;
-                    overflow: hidden; background-color: black;
+                    overflow: hidden; background-color: white;
                   }}
                   video {{
                     object-fit: cover; width: 100%; height: 100%;
--- obj/Debug/net8.0/MemeFinder.AssemblyInfo.cs
+++ obj/Debug/net8.0/MemeFinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MemeFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3aa9486adeb9d64d65ffa88fdc7d165e46d5cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8a2a1786ae6151663cd7fbb69faa58a5214d09e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MemeFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MemeFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
